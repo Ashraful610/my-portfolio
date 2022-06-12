2999948739d3cd608a7512fef6462bcf5754bd87
Ashraful610 <ashalam610@gmail.com>
2022-06-12 15:27:20 +0600

    updated

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -12,7 +12,6 @@ const Banner = () => {
                     <h2  className='text-2xl'>Ashraful  Alam</h2>
                     <h2  className='text-2xl'>Web Developer</h2>
                     <p className='pe-4 text-2xl'>I like to code things from scratch, and enjoy bringing ideas to life in the browser.</p>
-                    <p>Finalize your Developer Portfolio website</p>
                     <div>
                          <button className="resume-btn">
                              <a href="https://i.ibb.co/8McYP0W/Resume-3-1.jpg" download alt="" className="lg:text-2xl text-xl">
